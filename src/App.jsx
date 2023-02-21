d8e440cf854b4917b9ea2bae1e32367f8646a6c8
royzce <roycemontoya77@gmail.com>
2023-02-21 12:14:45 +0800

    added-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return <RegisterPage />;
+  return (
+    <Routes>
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from "react-router-dom";
 import { RegisterProvider } from "./context/RegisterContext";
 
