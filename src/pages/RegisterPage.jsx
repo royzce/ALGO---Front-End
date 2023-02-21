749574bf8e386672379d6860bde847138d46dd39
daryyyyl <daryl.victa15@gmail.com>
2023-02-21 11:22:48 +0800

    Design done

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,16 @@
 import {
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Grid,
+  IconButton,
+  Paper,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
 import Joi from "joi";
 import { joiPasswordExtendCore } from "joi-password";
-import FirebaseProfileUpload from "../components/FirebaseProfileUpload";
+import appLogo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const joiPassword = Joi.extend(joiPasswordExtendCore);
 const RegisterPage = () => {
@@ -22,7 +20,7 @@ const RegisterPage = () => {
     username: "",
     email: "",
     password: "",
-    avatar: "",
+    // photoscr:""
   });
   const [errors, setErrors] = useState({});
 
@@ -71,110 +69,164 @@ const RegisterPage = () => {
 
     return !!result.error;
   };
-  const [imageUrl, setImageUrl] = useState("");
-  const [image, setImage] = useState(null);
 
-  function handleImageChange(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setImage(reader.result);
-      setImageUrl(reader.result);
-    };
-    reader.readAsDataURL(file);
-  }
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
+  const handlePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const styles = {
+    myTextField: {
+      "& .MuiFilledInput-root": {
+        backgroundColor: "rgb(248, 250,252)",
+        border: "1px solid #e2e2e1",
+        overflow: "hidden",
+        borderRadius: "12px",
+        "&:hover": {
+          backgroundColor: "transparent",
+        },
+        "&.Mui-focused": {
+          backgroundColor: "transparent",
+        },
+      },
+    },
+  };
   return (
-    <>
-      <Grid
-        container
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+        padding: "50px 0",
+        backgroundColor: "rgb(238,242,246)",
+      }}
+    >
+      <Paper
         component="form"
-        justifyContent="center"
+        autoComplete="off"
         onSubmit={handleSubmit}
+        elevation={0}
+        sx={{
+          borderRadius: "12px",
+          padding: "30px",
+          minWidth: "425px",
+          maxWidth: "685px",
+          my: "auto",
+        }}
       >
-        <Grid item xs={6}>
-          <Card>
-            <CardHeader title="Register" />
-            <CardContent>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <FirebaseProfileUpload />
-                  <Typography variant="caption" sx={{ fontStyle: "italic" }}>
-                    Choose Avatar (Optional)
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    name="firstname"
-                    error={!!errors.firstname}
-                    helperText={errors.firstname}
-                    onChange={handleChange}
-                    value={form.firstname}
-                    label="First Name"
-                    variant="standard"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    name="lastname"
-                    error={!!errors.lastname}
-                    helperText={errors.lastname}
-                    onChange={handleChange}
-                    value={form.lastname}
-                    label="Last Name"
-                    variant="standard"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    name="username"
-                    error={!!errors.username}
-                    helperText={errors.username}
-                    onChange={handleChange}
-                    value={form.username}
-                    label="Username"
-                    variant="standard"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    name="email"
-                    error={!!errors.email}
-                    helperText={errors.email}
-                    onChange={handleChange}
-                    value={form.email}
-                    label="Email"
-                    variant="standard"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    name="password"
-                    error={!!errors.password}
-                    helperText={errors.password}
-                    onChange={handleChange}
-                    value={form.password}
-                    type="password"
-                    label="Password"
-                    variant="standard"
-                    fullWidth
-                  />
-                </Grid>
-              </Grid>
-            </CardContent>
-            <CardActions>
-              <Button disabled={isFormInvalid()} type="submit" fullWidth>
-                Sign Up
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </>
+        <img
+          src={appLogo}
+          className="rounded mx-auto d-block"
+          style={{ height: "8vh", marginTop: "26px" }}
+          alt="Algo app logo"
+        />
+        <Typography variant="h5" sx={{ textAlign: "center", margin: "26px 0" }}>
+          <strong>Sign Up</strong>
+        </Typography>
+        <Typography
+          className="text-secondary mb-4"
+          variant="body2"
+          sx={{ textAlign: "center", mt: "15px" }}
+        >
+          Enter your credentials to join the ALGO community
+        </Typography>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing={1}
+          sx={{ mb: "10px" }}
+        >
+          <TextField
+            name="firstname"
+            error={!!errors.firstname}
+            helperText={errors.firstname}
+            onChange={handleChange}
+            value={form.firstname}
+            label="First Name"
+            variant="filled"
+            InputProps={{ disableUnderline: true }}
+            sx={styles.myTextField}
+            fullWidth
+          />
+          <TextField
+            name="lastname"
+            error={!!errors.lastname}
+            helperText={errors.lastname}
+            onChange={handleChange}
+            value={form.lastname}
+            label="Last Name"
+            variant="filled"
+            InputProps={{ disableUnderline: true }}
+            sx={styles.myTextField}
+            fullWidth
+          />
+        </Stack>
+        <TextField
+          name="username"
+          error={!!errors.username}
+          helperText={errors.username}
+          onChange={handleChange}
+          value={form.username}
+          label="Username"
+          variant="filled"
+          InputProps={{ disableUnderline: true }}
+          sx={[styles.myTextField, { mb: "10px" }]}
+          fullWidth
+        />
+        <TextField
+          name="email"
+          error={!!errors.email}
+          helperText={errors.email}
+          onChange={handleChange}
+          value={form.email}
+          label="Email"
+          variant="filled"
+          InputProps={{ disableUnderline: true }}
+          sx={[styles.myTextField, { mb: "10px" }]}
+          fullWidth
+        />
+        <IconButton
+          size="small"
+          className="text-secondary bg-transparent float-end border-0"
+          onClick={handlePasswordVisibility}
+          sx={[
+            passwordVisible ? { marginRight: "14px" } : { marginRight: "15px" },
+            { zIndex: 3, mt: "16px" },
+          ]}
+        >
+          <i
+            className={
+              passwordVisible ? "fa-solid fa-eye-slash" : "fa-solid fa-eye" // font awesome icon
+            }
+          />
+        </IconButton>
+        <TextField
+          name="password"
+          error={!!errors.password}
+          helperText={errors.password}
+          onChange={handleChange}
+          value={form.password}
+          type={passwordVisible ? "text" : "password"}
+          label="Password"
+          variant="filled"
+          InputProps={{ disableUnderline: true }}
+          sx={[styles.myTextField, { mb: "10px", mt: "-44px" }]}
+          fullWidth
+        />
+        <Button
+          disabled={isFormInvalid()}
+          type="submit"
+          variant="contained"
+          fullWidth
+        >
+          Sign Up
+        </Button>
+        <hr />
+        <Typography variant="body2" sx={{ textAlign: "center" }}>
+          <Link to="/login">Already have an account?</Link>
+        </Typography>
+      </Paper>
+    </div>
   );
 };
 
