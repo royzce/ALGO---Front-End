f7766cdaf8b8375c3adafe0766e8abda436e0906
daryyyyl <daryl.victa15@gmail.com>
2023-02-22 14:05:49 +0800

    Loading backdrop, fixed doubling scroll bar, background color, added global css jsx

---
+++ src/components/GlobalCSS.jsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalCSS = createGlobalStyle`
+  body {
+    background-color: #eef2f6;
+  }
+`;
+
+export default GlobalCSS;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,6 +11,7 @@ import {
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import appLogo from "../assets/logo.png";
+import GlobalCSS from "../components/GlobalCSS";
 
 export default function Login() {
   const styles = {
@@ -28,6 +29,13 @@ export default function Login() {
         },
       },
     },
+    paper: {
+      borderRadius: "12px",
+      padding: "30px",
+      minWidth: "425px",
+      maxWidth: "685px",
+      margin: "70px auto",
+    },
   };
   const navigate = useNavigate();
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -35,104 +43,85 @@ export default function Login() {
     setPasswordVisible(!passwordVisible);
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "100vh",
-        padding: "50px 0",
-        backgroundColor: "rgb(238,242,246)",
-        overflowY: "scroll",
-      }}
-    >
-      <Paper
-        elevation={0}
-        sx={{
-          borderRadius: "12px",
-          padding: "30px",
-          minWidth: "425px",
-          my: "auto",
-          maxWidth: "425px",
-        }}
+    <Paper elevation={0} style={styles.paper}>
+      <img
+        src={appLogo}
+        className="rounded mx-auto d-block"
+        style={{ height: "8vh", marginTop: "26px" }}
+        alt="Algo app logo"
+      />
+      <GlobalCSS />
+      <Typography variant="h5" sx={{ textAlign: "center", margin: "26px 0" }}>
+        <strong>Hi, Welcome to Algo</strong>
+      </Typography>
+      <TextField
+        name="email"
+        // error={!!errors.email}
+        // helperText={errors.email}
+        // onChange={handleChange}
+        // value={form.email}
+        label="Email / Username"
+        variant="filled"
+        InputProps={{ disableUnderline: true }}
+        sx={[styles.myTextField, { mb: "10px" }]}
+        fullWidth
+      />
+      <IconButton
+        size="small"
+        className="text-secondary bg-transparent float-end border-0"
+        onClick={handlePasswordVisibility}
+        sx={[
+          passwordVisible ? { marginRight: "14px" } : { marginRight: "15px" },
+          { zIndex: 3, mt: "16px" },
+        ]}
       >
-        <img
-          src={appLogo}
-          className="rounded mx-auto d-block"
-          style={{ height: "8vh", marginTop: "26px" }}
-          alt="Algo app logo"
+        <i
+          className={
+            passwordVisible ? "fa-solid fa-eye-slash" : "fa-solid fa-eye" // font awesome icon
+          }
         />
-        <Typography variant="h5" sx={{ textAlign: "center", margin: "26px 0" }}>
-          <strong>Hi, Welcome to Algo</strong>
+      </IconButton>
+      <TextField
+        name="password"
+        // error={!!errors.password}
+        // helperText={errors.password}
+        // onChange={handleChange}
+        // value={form.password}
+        type={passwordVisible ? "text" : "password"}
+        label="Password"
+        variant="filled"
+        InputProps={{ disableUnderline: true }}
+        sx={[styles.myTextField, { mb: "10px", mt: "-44px" }]}
+        fullWidth
+      />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="baseline"
+        spacing={0}
+      >
+        <FormControlLabel
+          control={<Checkbox size="small" />}
+          label={<Typography variant="body2">Remember me</Typography>}
+        />
+        <Typography variant="body2">
+          <Link id="forgot" to="#" className="float-end">
+            Forgot password?
+          </Link>
         </Typography>
-        <TextField
-          name="email"
-          // error={!!errors.email}
-          // helperText={errors.email}
-          // onChange={handleChange}
-          // value={form.email}
-          label="Email / Username"
-          variant="filled"
-          InputProps={{ disableUnderline: true }}
-          sx={[styles.myTextField, { mb: "10px" }]}
-          fullWidth
-        />
-        <IconButton
-          size="small"
-          className="text-secondary bg-transparent float-end border-0"
-          onClick={handlePasswordVisibility}
-          sx={[
-            passwordVisible ? { marginRight: "14px" } : { marginRight: "15px" },
-            { zIndex: 3, mt: "16px" },
-          ]}
-        >
-          <i
-            className={
-              passwordVisible ? "fa-solid fa-eye-slash" : "fa-solid fa-eye" // font awesome icon
-            }
-          />
-        </IconButton>
-        <TextField
-          name="password"
-          // error={!!errors.password}
-          // helperText={errors.password}
-          // onChange={handleChange}
-          // value={form.password}
-          type={passwordVisible ? "text" : "password"}
-          label="Password"
-          variant="filled"
-          InputProps={{ disableUnderline: true }}
-          sx={[styles.myTextField, { mb: "10px", mt: "-44px" }]}
-          fullWidth
-        />
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="baseline"
-          spacing={0}
-        >
-          <FormControlLabel
-            control={<Checkbox size="small" />}
-            label={<Typography variant="body2">Remember me</Typography>}
-          />
-          <Typography variant="body2">
-            <Link id="forgot" to="#" className="float-end">
-              Forgot password?
-            </Link>
-          </Typography>
-        </Stack>
-        <Button variant="contained" size="medium" className="mt-3" fullWidth>
-          Log In
-        </Button>
-        <hr />
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => navigate("/register")}
-          fullWidth
-        >
-          Create an account
-        </Button>
-      </Paper>
-    </div>
+      </Stack>
+      <Button variant="contained" size="medium" className="mt-3" fullWidth>
+        Log In
+      </Button>
+      <hr />
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => navigate("/register")}
+        fullWidth
+      >
+        Create an account
+      </Button>
+    </Paper>
   );
 }
