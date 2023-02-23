1fa43c194ad9795ce380bf6f54c1541ae12af55a
royzce <roycemontoya77@gmail.com>
2023-02-24 00:22:18 +0800

    added-feature

--- src/services/search.js
+++ src/services/search.js
@@ -3,7 +3,3 @@ import { http } from "./http";
 export function getUsersBy(name) {
   return http.get(`/users/search?q=${name}`);
 }
-
-export function getAllUser() {
-  return http.get("/users/all");
-}
